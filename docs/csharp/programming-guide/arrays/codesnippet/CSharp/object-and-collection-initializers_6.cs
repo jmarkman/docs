5e512bb6d70c4e75ae8dbe7a5315a4edff296f4e
jmarkman <jmarkman446@gmail.com>
2018-12-20 00:36:51 -0500

    finished multi-nested object initializer example added ToString method for proposed Shelter class

--- docs/csharp/programming-guide/arrays/codesnippet/CSharp/object-and-collection-initializers_6.cs
+++ docs/csharp/programming-guide/arrays/codesnippet/CSharp/object-and-collection-initializers_6.cs
@@ -20,7 +20,22 @@
                     // Auto-implemented properties.
                     public Cat CatMascot { get; set; }
                     public List<Cat> Cats { get; set; }
-                    public string SupervisorName { get; set; }
+                    public Person Supervisor { get; set; }
+
+                    public override string ToString()
+                    {
+                        StringBuilder strBuilder = new StringBuilder();
+                        strBuilder.Append($"The shelter's mascot is {CatMascot.Name}, who is {CatMascot.Age} years old.");
+                        strBuilder.Append(Environment.NewLine);
+                        strBuilder.Append($"There are {Cats.Count} cats in this shelter. They are:");
+                        foreach (var cat in Cats)
+                        {
+                            strBuilder.Append(cat);
+                            strBuilder.Append(Environment.NewLine);
+                        }
+                        strBuilder.Append($"The supervisor of the shelter is {Supervisor.Name} and they own a cat named {Supervisor.PetCat.Name} who is {Supervisor.PetCat.Age} years old.");
+                        return strBuilder.ToString();
+                    }
                 }
 
                 static void Main()
@@ -28,26 +43,6 @@
                     // Initializing a new Cat object
                     Cat cat = new Cat { Age = 10, Name = "Fluffy" };
                     
-                    // Using nested object initialization with the Person class:
-                    // Initializing a new Person class requires initializing a new Cat
-                    Person person = new Person
-                    {
-                        PetCat = new Cat { Age = 2, Name = "Tiger" },
-                        Name = "John Smith"
-                    };
-
-                    // Multiple nested initialization in action
-                    Shelter catShelter = new Shelter
-                    {
-                        CatMascot = new Cat { Age = 4, Name = "Blacktip"},
-                        Cats = new List<Cat>
-                        {
-                            new Cat { Age = 5, Name = "Tom" },
-                            new Cat { Age = 3, Name = "Sakamoto" }
-                        },
-                        SupervisorName = "Sarah Katz"
-                    };
-
                     List<Cat> cats = new List<Cat>
                     {
                         new Cat(){ Name = "Sylvester", Age=8 },
@@ -62,24 +57,54 @@
                         null
                     };
 
+                    // Using nested object initialization with the Person class:
+                    // Initializing a new Person class requires initializing a new Cat
+                    Person person = new Person
+                    {
+                        PetCat = new Cat { Age = 2, Name = "Tiger" },
+                        Name = "John Smith"
+                    };
+
+                    // Multiple nested initialization in action. Start by initializing a new Shelter:
+                    Shelter catShelter = new Shelter
+                    {
+                        // Every good shelter needs a mascot, so we can create a new Cat for that:
+                        CatMascot = new Cat { Age = 4, Name = "Blacktip"},
+
+                        // What good is a shelter if no cats live in it? We can initialize a new list of cats
+                        Cats = 
+                        {
+                            new Cat { Age = 5, Name = "Tom" },
+                            new Cat { Age = 3, Name = "Sakamoto" }
+                        },
+                        // And someone's gotta lead the employees of the shelter!
+                        Supervisor = new Person
+                        {
+                            PetCat = new Cat { Age = 6, Name = "Bruno" },
+                            Name = "Sarah Katz"
+                        }
+                    };
+
                     // Display results.
-                    System.Console.WriteLine(cat.Name);
+                    System.Console.WriteLine($"{cat.Name} is {cat.Age} years old.");
 
                     foreach (Cat c in cats)
-                        System.Console.WriteLine(c.Name);
+                        System.Console.WriteLine($"{c.Name} is {c.Age} years old.");
 
                     foreach (Cat c in moreCats)
                         if (c != null)
-                            System.Console.WriteLine(c.Name);
+                            System.Console.WriteLine($"{c.Name} is {c.Age} years old.");
                         else
                             System.Console.WriteLine("List element has null value.");
+
+                    System.Console.WriteLine(catShelter.ToString());
                 }
                 // Output:
-                //Fluffy
-                //Sylvester
-                //Whiskers
-                //Sasha
-                //Furrytail
-                //Peaches
+                //Fluffy is 10 years old.
+                //Sylvester is 8 years old.
+                //Whiskers is 2 years old.
+                //Sasha is 14 years old.
+                //Furrytail is 5 years old.
+                //Peaches is 4 years old.
                 //List element has null value.
             }
